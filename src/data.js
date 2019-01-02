8c5faecf3e180a4cba554c1d4717d15397622760
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-01-02 08:57:02 -0300

    Portada del sitio + mostrar data completo

--- src/data.js
+++ src/data.js
@@ -1,5 +1,20 @@
 const dataInjuries = INJURIES; 
-const selectYear = (dataInjuries) =>{ 
+let groupAccidentsAcuatics = [];
+let groupAccidentsAir = [];
+let groupAccidentsPedestrian = [];
+let groupAccidentsPipper = [];
+let groupAccidentsOther = [];
+let groupAccidentsHighway = [];
+let groupAccidentsEmployee = [];
+let groupAccidentsRailway = [];
+let groupAccidentsPrivateVehicles = [];
+for(let i=0; i<dataInjuries.length; i++){
+  Object.keys(dataInjuries[i]).forEach(function(key) {
+    console.log(`${key.replace(/_/g, " ")} ${dataInjuries[i][key]}`); 
+});
+};
+
+/*const selectYear = (dataInjuries) =>{ 
   let years = []; 
   for (let i = 0; i<dataInjuries.length; i++){
     years.push(dataInjuries[i].Year)
@@ -7,7 +22,18 @@ const selectYear = (dataInjuries) =>{
   return years; 
 }
 window.selectYear = selectYear; 
-let selectYearToFilter = () =>{ 
-  
+let selectYearToFilter = (dataInjuries) =>{ 
+const prueba = dataInjuries.filter(dataInjuries => {
+    return (dataInjuries.Year) === selected;
+});
+console.log(prueba);
 }
-window.selectYearToFilter = selectYearToFilter;
+window.selectYearToFilter = selectYearToFilter;*/
+/*const dataInjuries = INJURIES;
+const showAllData = (dataInjuries) =>{
+  let allData = [];
+  for (let i=0; i<dataInjuries.length; i++){
+    allData.push(dataInjuries[i])
+  }
+  return allData;
+}*/
--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-const yearToFilter = selectYear(dataInjuries);
+/*const yearToFilter = selectYear(dataInjuries);
 document.getElementById('year_list').innerHTML = '';
 for (let i = 0; i < yearToFilter.length; i++){
-    document.getElementById('year_list').innerHTML += '<option>' + String(yearToFilter[i]) + '</option>'; 
+    document.getElementById('year_list').innerHTML += '<option>' + (yearToFilter[i]) + '</option>'; 
 };
 let optionSelect = document.getElementById('year_list');
   optionSelect.addEventListener('change',  function(){
-    let selectedOption = this.options[optionSelect.selectedIndex];
-    let optionValue = (selectedOption.text); 
-    console.log(optionValue);
-  });
-const actionFilterToYear = document.getElementById('button_filter_year').addEventListener('click', (actionfilter) =>{
+    let combo = document.getElementById("year_list");
+    let selected = combo.options[combo.selectedIndex].text;
+    console.log(selected);
     const valueYear = selectYearToFilter(dataInjuries);
-    document.getElementById('result_year_filtered').innerHTML = '<div>' + valueYear + '</div>';   
+  document.getElementById('result_year_filtered').innerHTML = '<div>' + valueYear + '</div>'; 
   });
 
-
- 
+const actionFilterToYear = document.getElementById('button_filter_year').addEventListener('click', () =>{
+  
+});*/
+/** const showData = showAllData(dataInjuries);
+document.getElementById('type_of_accident').innerHTML = '';
+for (let i=0; i<showData.length; i++){
+  document.getElementById('type_of_accident').innerHTML += '<div>' + showData[i].Year + '</div>' + '<div>' + showData[i].Total_Injured_Persons_Water + '</div>';
+}*/
 
