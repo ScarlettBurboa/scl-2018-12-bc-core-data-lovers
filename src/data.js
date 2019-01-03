ae2cb5a494f1340034f9990404f031ceb4cefb45
DianaPfeiffer <diana_pfeiffer83@yahoo.es>
2019-01-03 08:26:24 -0300

    agregando cambio para pull request

--- src/data.js
+++ src/data.js
@@ -1,8 +1,23 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example; */
+
+/*const dataInjuries = INJURIES;
+const yearsOfAccidents = dataInjuries.filter(dataInjuries => {
+  return dataInjuries;
+});
+console.log(yearsOfAccidents);*/
+
+const datacategories = INJURIES;
+const acuatics = [];
+for(let i=0; i<datacategories.length;i++){
+  datacategories.map(datacategories => {
+    return datacategories;
+  });
+}
+console.log(acuatics);
