82498921508abc93219fac111fc3c2508a3984e5
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-01-10 22:42:32 -0300

    Categoría Accidentes Acuaticos Ok, más cambio de imagen principal, arreglo de menú y parte de responsivo

--- src/main.js
+++ src/main.js
@@ -63,52 +63,6 @@ document.getElementById('freight-vessel').addEventListener('click', ()=>{
     const selectBySort = document.getElementById('toFilter');
     selectBySort.classList.add('hide');
   });
-
-});
-const TotalSumInjuriedVesselRelated = sumAccidentsWaterVesselRelated;
-document.getElementById('water-vessel').addEventListener('click', () =>{
-  document.getElementById('totalSumofAccidentsWater').innerHTML = '';
-  document.getElementById('totalSumofAccidentsWater').innerHTML = `Total people injured in related accidents: ${TotalSumInjuriedVesselRelated}`
-  document.getElementById('showDataSelect').innerHTML = '';
-  for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayFreightVessel){
-    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayFreightVessel[i]} people injured by this type of accident.</li>`
-  }
-});
-const TotalSumInjuriedNotRelatedToVessel = sumAccidentsNotRelatedToVessel;
-document.getElementById('water-not-vessel').addEventListener('click', () =>{
-  document.getElementById('totalSumofAccidentsWater').innerHTML = '';
-  document.getElementById('totalSumofAccidentsWater').innerHTML = `Total people injured in related accidents: ${TotalSumInjuriedNotRelatedToVessel}`
-  document.getElementById('showDataSelect').innerHTML = '';
-  for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayNotRelatedToVessel){
-    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayNotRelatedToVessel[i]} people injured by this type of accident.</li>`
-  }
-});
-const TotalSumInjuriedAccidentsInWater = sumAccidentsInWater;
-document.getElementById('water').addEventListener('click', () =>{
-  document.getElementById('totalSumofAccidentsWater').innerHTML = '';
-  document.getElementById('totalSumofAccidentsWater').innerHTML = `Total people injured in related accidents: ${TotalSumInjuriedAccidentsInWater}`
-  document.getElementById('showDataSelect').innerHTML = '';
-  for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayWater){
-    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayWater[i]} people injured by this type of accident.</li>`
-  }
-});
-const TotalSumInjuriedRecreationalBoating = sumAccidentsRecreationalBoating;
-document.getElementById('recreational-boating').addEventListener('click', () =>{
-  document.getElementById('totalSumofAccidentsWater').innerHTML = '';
-  document.getElementById('totalSumofAccidentsWater').innerHTML = `Total people injured in related accidents: ${TotalSumInjuriedRecreationalBoating}`
-  document.getElementById('showDataSelect').innerHTML = '';
-  for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayRecreationalBoating){
-    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayRecreationalBoating[i]} people injured by this type of accident.</li>`
-  }
-});
-const TotalSumInjuriedPassengerVessel = sumAccidentsPassengerVessel;
-document.getElementById('passenger-vessel').addEventListener('click', () =>{
-  document.getElementById('totalSumofAccidentsWater').innerHTML = '';
-  document.getElementById('totalSumofAccidentsWater').innerHTML = `Total people injured in related accidents: ${TotalSumInjuriedPassengerVessel}`
-  document.getElementById('showDataSelect').innerHTML = '';
-  for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayPassengerVessel){
-    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayPassengerVessel[i]} people injured by this type of accident.</li>`
-  }
 });
 const selectYear = document.getElementById('filterYear');
 selectYear.addEventListener('change', () => {
@@ -127,4 +81,119 @@ selectSort.addEventListener('change', () =>{
     showDataSelect.innerHTML += `<li>On the date ${element.Year} there were ${element.Total_Injured_Persons_Freight_Vessel} people injured by this type of accident.</li>` 
   }) 
 });
-/** */
+const TotalSumInjuriedWaterVesselRelated = sumAccidentsWaterVesselRelated;
+const promedyInjuriedWaterVesselRelated = promedyWaterVesselRelated; 
+const medianInjuriedWaterVesselRelated = resultMedianWaterVesselRelated;
+const numHightInjuriedWaterVesselRelated = funnumHighWaterVesselRelated;
+const numLowInjuriedWaterVesselRelated = funnumLowWaterVesselRelated;
+document.getElementById('water-vessel').addEventListener('click', ()=>{
+  const showSelectFilterAndSort = document.getElementById('showData');
+  showSelectFilterAndSort.classList.add('show');
+  document.getElementById('totalSumofAccidents').innerHTML = '';
+  document.getElementById('totalSumofAccidents').innerHTML = `<span>Total number of people injured from 1960 to 2016: <b>${TotalSumInjuriedWaterVesselRelated}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Average from 1960 to 2016 of injuries: <b>${promedyInjuriedWaterVesselRelated}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Arithmetic median of the wounded people from 1960 to 2016: <b>${medianInjuriedWaterVesselRelated}</b></span>`;
+  document.getElementById('numHightAccidents').innerHTML = '';  
+  document.getElementById('numHightAccidents').innerHTML = `<span>The largest number of injured between 1960 and 2016: <b>${numHightInjuriedWaterVesselRelated}</b> of the year <b>2003</b></span>`;
+  document.getElementById('numLeastAccidents').innerHTML = '';  
+  document.getElementById('numLeastAccidents').innerHTML = `<span>The least number of injured between 1960 and 2016: <b>${numLowInjuriedWaterVesselRelated}</b> from <b>1960 to 2001</b></span>`; 
+  document.getElementById('showDataSelect').innerHTML = '';
+   for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayWaterVesselRelated){
+    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayWaterVesselRelated[i]} people injured by this type of accident.</li>`
+  }
+});
+const TotalSumInjuriedNotRelatedToVessel = sumAccidentsNotRelatedToVessel;
+const promedyInjuriedNotRelatedToVessel = promedyNotRelatedToVessel;
+const medianInjuriedNotRelatedToVessel = resultMedianNotRelatedToVessel;
+const numLowInjuriedNotRelatedToVessel = funnumLowNotRelatedToVessel;
+const numHightInjuriedNotRelatedToVessel = funnumHighNotRelatedToVessel;
+document.getElementById('water-not-vessel').addEventListener('click', ()=>{
+  const showSelectFilterAndSort = document.getElementById('showData');
+  showSelectFilterAndSort.classList.add('show');
+  document.getElementById('totalSumofAccidents').innerHTML = '';
+  document.getElementById('totalSumofAccidents').innerHTML = `<span>Total number of people injured from 1960 to 2016: <b>${TotalSumInjuriedNotRelatedToVessel}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Average from 1960 to 2016 of injuries: <b>${promedyInjuriedNotRelatedToVessel}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Arithmetic median of the wounded people from 1960 to 2016: <b>${medianInjuriedNotRelatedToVessel}</b></span>`;
+  document.getElementById('numHightAccidents').innerHTML = '';  
+  document.getElementById('numHightAccidents').innerHTML = `<span>The largest number of injured between 1960 and 2016: <b>${numHightInjuriedNotRelatedToVessel}</b> of the year <b>2003</b></span>`;
+  document.getElementById('numLeastAccidents').innerHTML = '';  
+  document.getElementById('numLeastAccidents').innerHTML = `<span>The least number of injured between 1960 and 2016: <b>${numLowInjuriedNotRelatedToVessel}</b> from <b>1960 to 2001</b></span>`; 
+  document.getElementById('showDataSelect').innerHTML = '';
+   for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayNotRelatedToVessel){
+    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayNotRelatedToVessel[i]} people injured by this type of accident.</li>`
+  }
+});
+const TotalSumInjuriedWater = sumAccidentsInWater;
+const promedyInjuriedWater = promedyWater;
+const medianInjuriedWater = resultMedianWater;
+const numHightInjuriedWater = funnumHighWater;
+const numLowInjuriedWater = funnumLowWater;
+document.getElementById('water').addEventListener('click', ()=>{
+  const showSelectFilterAndSort = document.getElementById('showData');
+  showSelectFilterAndSort.classList.add('show');
+  document.getElementById('totalSumofAccidents').innerHTML = '';
+  document.getElementById('totalSumofAccidents').innerHTML = `<span>Total number of people injured from 1960 to 2016: <b>${TotalSumInjuriedWater}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Average from 1960 to 2016 of injuries: <b>${promedyInjuriedWater}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Arithmetic median of the wounded people from 1960 to 2016: <b>${medianInjuriedWater}</b></span>`;
+  document.getElementById('numHightAccidents').innerHTML = '';  
+  document.getElementById('numHightAccidents').innerHTML = `<span>The largest number of injured between 1960 and 2016: <b>${numHightInjuriedWater}</b> of the year <b>2003</b></span>`;
+  document.getElementById('numLeastAccidents').innerHTML = '';  
+  document.getElementById('numLeastAccidents').innerHTML = `<span>The least number of injured between 1960 and 2016: <b>${numLowInjuriedWater}</b> from <b>1960 to 2001</b></span>`; 
+  document.getElementById('showDataSelect').innerHTML = '';
+   for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayWater){
+    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayWater[i]} people injured by this type of accident.</li>`
+  }
+});
+const TotalSumInjuriedRecreationalBoating = sumAccidentsRecreationalBoating;
+const promedyInjuriedRecreationalBoating = promedyRecreationalBoating ;
+const medianInjuriedRecreationalBoating = resultMedianRecreationalBoating;
+const numHightInjuriedRecreationalBoating = funnumHighRecreationalBoating ;
+const numLowInjuriedRecreationalBoating = funnumLowRecreationalBoating;
+document.getElementById('recreational-boating').addEventListener('click', ()=>{
+  const showSelectFilterAndSort = document.getElementById('showData');
+  showSelectFilterAndSort.classList.add('show');
+  document.getElementById('totalSumofAccidents').innerHTML = '';
+  document.getElementById('totalSumofAccidents').innerHTML = `<span>Total number of people injured from 1960 to 2016: <b>${TotalSumInjuriedRecreationalBoating}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Average from 1960 to 2016 of injuries: <b>${promedyInjuriedRecreationalBoating}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Arithmetic median of the wounded people from 1960 to 2016: <b>${medianInjuriedRecreationalBoating}</b></span>`;
+  document.getElementById('numHightAccidents').innerHTML = '';  
+  document.getElementById('numHightAccidents').innerHTML = `<span>The largest number of injured between 1960 and 2016: <b>${numHightInjuriedRecreationalBoating}</b> of the year <b>2003</b></span>`;
+  document.getElementById('numLeastAccidents').innerHTML = '';  
+  document.getElementById('numLeastAccidents').innerHTML = `<span>The least number of injured between 1960 and 2016: <b>${numLowInjuriedRecreationalBoating}</b> from <b>1960 to 2001</b></span>`; 
+  document.getElementById('showDataSelect').innerHTML = '';
+   for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayRecreationalBoating){
+    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayRecreationalBoating[i]} people injured by this type of accident.</li>`
+  }
+});
+const TotalSumInjuriedPassengerVessel = sumAccidentsPassengerVessel;
+const promedyInjuriedPassengerVessel = promedyPassengerVessel ;
+const medianInjuriedPassengerVessel = resultMedianPassengerVessel;
+const numHightInjuriedPassengerVessel = funnumHighPassengerVessel ;
+const numLowInjuriedPassengerVessel = funnumLowPassengerVessel;
+document.getElementById('passenger-vessel').addEventListener('click', ()=>{
+  const showSelectFilterAndSort = document.getElementById('showData');
+  showSelectFilterAndSort.classList.add('show');
+  document.getElementById('totalSumofAccidents').innerHTML = '';
+  document.getElementById('totalSumofAccidents').innerHTML = `<span>Total number of people injured from 1960 to 2016: <b>${TotalSumInjuriedPassengerVessel}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Average from 1960 to 2016 of injuries: <b>${promedyInjuriedPassengerVessel}</b></span>`;
+  document.getElementById('promedyAccidents').innerHTML = '';
+  document.getElementById('promedyAccidents').innerHTML = `<span>Arithmetic median of the wounded people from 1960 to 2016: <b>${medianInjuriedPassengerVessel}</b></span>`;
+  document.getElementById('numHightAccidents').innerHTML = '';  
+  document.getElementById('numHightAccidents').innerHTML = `<span>The largest number of injured between 1960 and 2016: <b>${numHightInjuriedPassengerVessel}</b> of the year <b>2003</b></span>`;
+  document.getElementById('numLeastAccidents').innerHTML = '';  
+  document.getElementById('numLeastAccidents').innerHTML = `<span>The least number of injured between 1960 and 2016: <b>${numLowInjuriedPassengerVessel}</b> from <b>1960 to 2001</b></span>`; 
+  document.getElementById('showDataSelect').innerHTML = '';
+   for(i in resultArrayCategoryWater.arrayYear && resultArrayCategoryWater.arrayPassengerVessel){
+    document.getElementById('showDataSelect').innerHTML += `<li>On the date ${resultArrayCategoryWater.arrayYear[i]} there were ${resultArrayCategoryWater.arrayPassengerVessel[i]} people injured by this type of accident.</li>`
+  }
+});
+
